059417ce99f77f1616a48344cd87dcfbb83ffcd2
tristan <thuckabee.work@gmail.com>
2023-03-08 18:19:11 -0600

    finished fetchLong

--- fetchLong/fetch-requests.js
+++ fetchLong/fetch-requests.js
@@ -4,17 +4,41 @@ Paste your code for fetch requests here once you finish each phase.
 */
 
 /* ============================== Phase 1 ============================== */
-
-// Your code here
-
-
-
+fetch("/products", {
+  method: "POST",
+  headers: {
+    "Content-Type": "application/x-www-form-urlencoded"
+  },
+  body: "name=Caribbean+Delight+Coffee&description=Made+by+Manatee+Coffee&price=11%2E99&categories=grocery"
+});
 /* ============================== Phase 2 ============================== */
-
-// Your code here
-
-
-
+fetch("/products", {
+  method: "POST",
+  headers: {
+    "Content-Type": "application/x-www-form-urlencoded"
+  },
+  body: "name=Caribbean+Delight+Coffee&description=Made+by+Manatee+Coffee&price=11%2E99&categories=grocery"
+})
+.then(res => {
+  console.log(res.status);
+  console.log(res.headers.get("Content-Type"));
+  console.log(res.url);
+});
 /* ============================== Phase 3 ============================== */
-
-// Your code here
+fetch("/products", {
+  method: "POST",
+  headers: {
+    "Content-Type": "application/x-www-form-urlencoded"
+  },
+  body: new URLSearchParams({
+    name: "Caribbean Delight Coffee",
+    description: "Made by Manatee Coffee",
+    price: 11.99,
+    categories: "grocery"
+  })
+})
+.then(res => {
+  console.log(res.status);
+  console.log(res.headers.get("Content-Type"));
+  console.log(res.url);
+});
